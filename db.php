d693331519edeeb7e9227e19d181451ab28eac2e
Juan Camilo Garcia <jcgm1047@gmail.com>
2021-11-09 09:25:05 -0500

    Carga de proyecto a git

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+$conn = mysqli_connect("localhost","root","","php_mysql_crud");
+if (isset($conn)) {
+
+    echo "db is connect";
+   
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include("./includes/header.php") ?>
+
+<H1>Hola mundo </H1>
+<h1>hola</h1>
+
+<?php include("./includes/footer.php") ?>
