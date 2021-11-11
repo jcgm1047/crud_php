4e5c5c8c37f76431e897c7986777d9974b53cf47
Juan <jcgm1047@gmail.com>
2021-11-11 17:50:38 -0500

    ultimos cambios

--- db.php
+++ db.php
@@ -1,16 +1,5 @@
 <?php
-<<<<<<< HEAD
-=======
-
-session_start();
-$conn = mysqli_connect("localhost","root","","php_mysql_crud");
->>>>>>> ff951836028b2b71b94c29b4d6549cae92a62c71
 
 session_start();
 
-<<<<<<< HEAD
 $conn = mysqli_connect("localhost","root","","php_mysql_crud");
-=======
-
-?>
->>>>>>> ff951836028b2b71b94c29b4d6549cae92a62c71
